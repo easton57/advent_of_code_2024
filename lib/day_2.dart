b24d11605783b174c5a2234002f5b233e0b015d5
Easton Seidel <eseidel1357@gmail.com>
2024-12-03 16:06:03 -0800

    Finished day 2 part 1, working on part 2

--- lib/day_2.dart
+++ lib/day_2.dart
@@ -2,13 +2,15 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
-void main() {
+void main() async {
   final inFile = File("inputs/day_2.txt");
 
   Stream<String> lines =
       inFile.openRead().transform(utf8.decoder).transform(LineSplitter());
 
-  print(isItSafe(lines));
+  int safe = await isItSafe2(lines);
+
+  print("Safe $safe");
 }
 
 Future<int> isItSafe(Stream<String> input) async {
@@ -18,18 +20,60 @@ Future<int> isItSafe(Stream<String> input) async {
   await for (String i in input) {
     var split = i.split(" ");
 
+    bool decreasing = false;
+
     for (int j = 1; j < split.length; j++) {
-      int diff = (int.parse(split[j - 1]) - int.parse(split[j])) * -1;
-
-      if (diff > 3) {
-        break;
+      int prev = int.parse(split[j - 1]);
+      int curr = int.parse(split[j]);
+      int diff = prev - curr;
+      if (j - 1 == 0 && prev > curr) {
+        decreasing = true;
       }
-      else {
-        if (j++ == split.length) {
-          // Check to make sure it's increasing or decreasing
 
-           safe++;
-        }
+      if (!decreasing) {
+        diff = diff * -1;
+      }
+
+      if (diff > 3 || diff < 1) {
+        break;
+      } else if (j + 1 == split.length) {
+        safe++;
+        decreasing = false;
+      }
+    }
+  }
+
+  return safe;
+}
+
+Future<int> isItSafe2(Stream<String> input) async {
+  int safe = 0;
+
+  // Time for a horrible loop
+  await for (String i in input) {
+    var split = i.split(" ");
+
+    bool decreasing = false;
+    bool bad_level_used = false;
+
+    for (int j = 1; j < split.length; j++) {
+      int prev = int.parse(split[j - 1]);
+      int curr = int.parse(split[j]);
+      int diff = prev - curr;
+      if (j - 1 == 0 && prev > curr) {
+        decreasing = true;
+      }
+
+      if (!decreasing) {
+        diff = diff * -1;
+      }
+
+      if (diff > 3 || diff < 1) {
+        break;
+      } else if (j + 1 == split.length) {
+        safe++;
+        decreasing = false;
+        bad_level_used = false;
       }
     }
   }
